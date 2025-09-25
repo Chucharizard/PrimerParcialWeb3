b9635dfa6b86acd00c6c5c43a59a1b9f9be2820c
Chucho <willyguevara048@gmail.com>
2025-09-25 19:26:49 -0400

    agregar el modelo Recipe

---
+++ PrimerParcial/Models/Recipe.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Instructions { get; set; }
+
+        public int PreparationTimeMinutes { get; set; }
+
+        public int Servings { get; set; }
+
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        // FK
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+        public ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
